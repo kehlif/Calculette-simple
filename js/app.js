db2fc32306f7b1d17edf99ed13ed25683f2bec82
kehli <fatima.kehili@hotmail.fr>
2018-01-01 20:14:18 +0100

    fin calculette simple

---
+++ js/app.js
@@ -0,0 +1,76 @@
+// /*jshint esversion: 6*/
+// var calculette = (function(){
+//   var res = document.getElementById('result');
+//
+//   const add = function add(){
+//     var toto = document.getElementById('n1').value;
+//     var titi = document.getElementById('n2').value;
+//     console.log(toto);
+//     var sum = parseInt(toto) + parseInt(titi);
+//     res.value = sum;
+//   };
+//
+// window.onload = function(){
+// var plus = document.getElementById('plus');
+// plus.onclick= add;
+// };
+// }());
+/*jshint esversion: 6*/
+
+function calc() {
+  var n1 = document.getElementById('n1').value;
+  var n2 = document.getElementById('n2').value;
+  var oper = document.getElementById('operateurs').value;
+
+  if(n1 === "" && n2 === ""){
+    var contain = document.getElementById('container');
+    contain.innerHTML = "Aucune valeur saisie!";
+    contain.style.color = "yellow";
+    document.getElementById('result').value = " ";
+    document.getElementById('n1').focus();
+  }
+
+  if (isNaN(n1) || isNaN(n2)) {
+    alert("ceci n'est pas un nombre");
+    document.getElementById('result').value = " ";
+    document.getElementById('n1').focus();
+    document.getElementById('n2').focus();
+  }
+
+  if(oper === ""){
+    document.getElementById('result').value = "";
+  }
+
+  if (oper === "+") {
+    if(n1 != "" && n2 != ""){
+    document.getElementById('result').value = parseFloat(n1) + parseFloat(n2);
+  }else{
+    document.getElementById('n1').focus();
+    alert("erreur de saisie");
+  }
+}
+
+  if (oper === "-") {
+    document.getElementById('result').value = parseFloat(n1) - parseFloat(n2);
+  }
+  if (oper === "*") {
+    document.getElementById('result').value = parseFloat(n1) * parseFloat(n2);
+  }
+
+  if (oper === "/") {
+    if (n2 == 0) {
+      alert("Division impossible!");
+      document.getElementById('result').value = "";
+      document.getElementById('n2').focus();
+    } else {
+      document.getElementById('result').value = n1 / n2;
+    }
+  }
+}
+
+window.onload = function() {
+  document.getElementById('equal').onclick = calc;
+  n1.value = "";
+  n2.value = "";
+  document.getElementById('result').value = "";
+};
